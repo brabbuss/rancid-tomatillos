967a7f0de34b09d85f6d9950a091c4f62170a9b9
Scott Brabson <scottbrabson@gmail.com>
2020-12-02 20:32:22 -0700

    Import routing and remove React.Fragment

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/MovieCard.scss";
+import { Link } from "react-router-dom";
 
 const MovieCard = props => {
   const {
@@ -12,23 +13,20 @@ const MovieCard = props => {
   } = props.data;
 
   return (
-    <React.Fragment>
-      <div style={{ flex: "13rem" }} className="card mt-3">
-        <img className="card-img-top" src={poster} alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">
-            {title} {(rating/10*100).toFixed() + '%'}
-          </h5>
-          {/* <p className="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </p> */}
-          <p className="card-text">
-            <small className="text-muted">Release Date {release_date}</small>
-          </p>
-        </div>
+    <div
+      style={{ flex: "13rem", cursor: "pointer" }}
+      className="card mt-3"
+      onClick={() => props.onRoute(id)}>
+      <img className="card-img-top" src={poster} alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">
+          {title} {((rating / 10) * 100).toFixed() + "%"}
+        </h5>
+        <p className="card-text">
+          <small className="text-muted">Release Date {release_date}</small>
+        </p>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
