81b6683fe73870ca05821a7d65815484d5bd5c68
Scott Brabson <scottbrabson@gmail.com>
2020-12-04 15:07:15 -0700

    Add API call method for App component

--- src/components/utilities/apiCalls.js
+++ src/components/utilities/apiCalls.js
@@ -1,6 +1,4 @@
-import axios from "axios";
-
-// why does this only work inside of the Movies component and not when imported?
+// why does axios only work inside of the Movies component and not when imported?
 
 // export const getMovieData = async () => {
 //   const movieData = await axios.get(
@@ -11,9 +9,17 @@ import axios from "axios";
 
 // why do I need to put async/await in both this function and inside the movies component method?
 
-export const getMovieData = async () => {
+export const getMovieDataAPI = async () => {
   const movieData = await fetch(
     "https://rancid-tomatillos.herokuapp.com/api/v2/movies"
   ).then(response => response.json());
   return movieData.movies;
+};
+
+export const getMovieDetailsAPI = async id => {
+  const movieDetails = await fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
+  ).then(response => response.json());
+  console.log(movieDetails)
+  return movieDetails.movie;
 };
