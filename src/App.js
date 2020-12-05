372d92c5a64bba5a244844ea3337dccc95c2ebcb
Scott Brabson <scottbrabson@gmail.com>
2020-12-05 15:59:45 -0700

    Add state props for error info, add ErrorPage cmpnt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Movies from "./components/Movies";
 import MovieDetails from "./components/MovieDetails";
+import ErrorPage from './components/ErrorPage'
 import { Route, Switch } from "react-router-dom";
 import { getMovieDetailsAPI } from "./components/utilities/apiCalls";
 
@@ -9,15 +10,17 @@ class App extends Component {
   state = {
     selectedMovie: {},
     selectedMovieVideo: '',
-    statusError: false
+    statusError: false,
+    statusErrorCode: ''
   };
 
   getMovieDetails = async id => {
-    this.setState({ selectedMovie: await getMovieDetailsAPI(id) });
+    this.setState({ statusError: false, selectedMovie: await getMovieDetailsAPI(id) });
   };
 
   handleError = errorCode => {
-    console.log(errorCode)
+    this.setState({statusError: true, statusErrorCode: errorCode})
+    console.log(errorCode, this.state)
   }
 
   render() {
@@ -28,6 +31,12 @@ class App extends Component {
             path="/movies/:movie_id"
             render={props => (
               <MovieDetails data={this.state.selectedMovie} {...props} />
+            )}
+          />
+          <Route
+            path="/error"
+            render={props => (
+              <ErrorPage errorCode={this.state.statusErrorCode} {...props} />
             )}
           />
           <Route
