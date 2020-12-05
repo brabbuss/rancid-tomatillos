bd45ca4b8a0b900fbb15d62fa6a0dc02fd0ac36b
Scott Brabson <scottbrabson@gmail.com>
2020-12-05 15:38:30 -0700

    Add error handling API call for movieData

--- src/components/utilities/apiCalls.js
+++ src/components/utilities/apiCalls.js
@@ -9,12 +9,31 @@
 
 // why do I need to put async/await in both this function and inside the movies component method?
 
+// export const getMovieDataAPI = async () => {
+//   const movieData = await fetch(
+//     "https://rancid-tomatillos.herokuapp.com/api/v2/movies"
+//   ).then(response => response.json());
+//   return movieData.movies;
+// };
+
+
 export const getMovieDataAPI = async () => {
-  const movieData = await fetch(
-    "https://rancid-tomatillos.herokuapp.com/api/v2/movies"
-  ).then(response => response.json());
-  return movieData.movies;
-};
+  const response = await fetch("https://httpstat.us/500");
+  // const response = await fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies");
+  if (response.status >= 200 && response.status <= 299) {
+    // No errors
+    const jsonResponse = await response.json();
+    return jsonResponse.movies
+  } else {
+    // Handle 500 errors
+    console.log(response);
+    console.log(
+      `Error! Code: ${response.status}
+There seems to be a problem with the server. Please refresh the page.
+      `)
+    return response.status
+  }
+}
 
 export const getMovieDetailsAPI = async id => {
   const movieDetails = await fetch(
