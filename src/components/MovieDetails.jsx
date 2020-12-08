2d0dbd2e5c97e18b70b1ef91303fc2147ce3884e
Scott Brabson <scottbrabson@gmail.com>
2020-12-07 18:01:18 -0700

    Add fxn to get movie video link and display video

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/MovieDetails.scss";
+import ReactPlayer from "react-player/youtube";
 
 const MovieDetails = props => {
   const {
@@ -13,9 +14,19 @@ const MovieDetails = props => {
     runtime,
     tagline,
     average_rating: rating,
-    // some movies only have one video! target 'trailer'
-    promoVideo, // if initial video cannot be retrieved, pick next video in array - options to view different trailers, etc.
+    selectedMovieVideos: videos,
   } = props.data;
+
+  // if initial video cannot be retrieved, pick next video in array -
+  // options to view different trailers, etc.
+  // some movies only have one video! target 'trailer'
+
+  function getVideo(type) {
+    const matchedVideo = videos.find(v => v.type === type);
+    return `https://www.youtube.com/watch?v=${matchedVideo.key}`;
+  }
+
+  console.log(props)
 
   return (
     <React.Fragment>
@@ -37,7 +48,12 @@ const MovieDetails = props => {
         {budget > 0 && <p> Budget: ${budget.toLocaleString()}</p>}
         {revenue > 0 && <p> Revenue: ${revenue.toLocaleString()}</p>}
         {runtime > 0 && <p>Runtime: {runtime} minutes</p>}
-        {promoVideo && console.log(promoVideo)}
+        {videos.length && (
+          <ReactPlayer
+            url={getVideo('Trailer')}
+            width='100%'
+          />
+        )}
       </div>
     </React.Fragment>
   );
