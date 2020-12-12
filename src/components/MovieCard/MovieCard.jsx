05a7212ebefb7b2003be4476795983dae1113bd4
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-12 14:09:04 -0700

    Add file for movie card component

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../css/MovieCard.scss";
+import { Link } from "react-router-dom";
+
+const MovieCard = props => {
+  const {
+    id,
+    poster_path: poster,
+    title,
+    average_rating: rating,
+    release_date,
+  } = props.data;
+
+  return (
+    <div
+      style={{ flex: "13rem", cursor: "pointer", border: "none" }}
+      className="card mt-3"
+      onClick={() => props.getMovieDetails(id)}>
+      <Link to={`/movies/${id}`}>
+        <img className="card-img-top" src={poster} alt={`${id} poster`} />
+      </Link>
+      <div className="card-body" className="text-dark">
+        <h5 className="card-title">
+          {title} {((rating / 10) * 100).toFixed() + "%"}
+        </h5>
+        <p className="card-text">
+          <small className="text-muted">Release Date {release_date}</small>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
---
+++ src/components/MovieCard/MovieCard.test.js
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import MovieCard from "./MovieCard";
+import { BrowserRouter } from "react-router-dom";
+jest.mock("./utilities/apiCalls.js");
+import { fakeMovieData } from "../../data/fakeMovieData";
+
+describe("MovieCard", () => {
+
+  it("should render correctly", () => {
+    const fakeMovie = fakeMovieData.movies[0];
+    const mockGetMovieDetails = jest.fn();
+
+    render(
+      <BrowserRouter>
+        <MovieCard
+          key={fakeMovieData.id}
+          data={fakeMovie}
+          getMovieDetails={mockGetMovieDetails}
+        />
+      </BrowserRouter>
+    );
+
+    expect(
+      screen.getByRole("heading", { name: /money plane 67%/i })
+    ).toBeInTheDocument();
+    expect(screen.getByText("Release Date 2020-09-29")).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: /694919 poster/i })
+    ).toBeInTheDocument();
+  }),
+  
+    it("should call get movie details with the correct params", () => {
+      const mockGetMovieDetails = jest.fn();
+      const fakeMovie = fakeMovieData.movies[0];
+
+      render(
+        <BrowserRouter>
+          <MovieCard
+            key={fakeMovie.id}
+            data={fakeMovie}
+            getMovieDetails={mockGetMovieDetails}
+          />
+        </BrowserRouter>
+      );
+
+      userEvent.click(screen.getByRole("link", { name: /694919 poster/i }));
+      expect(mockGetMovieDetails).toHaveBeenCalledWith(fakeMovie.id);
+    });
+});
