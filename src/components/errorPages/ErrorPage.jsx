ce0647f33c1c343eee809741cb92afa6222fef10
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-12 14:07:38 -0700

    Add error code for 404 error

--- src/components/errorPages/ErrorPage.jsx
+++ src/components/errorPages/ErrorPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = ({errorCode}) => {
+  const otherError = 
+    <p>Sorry, there was an error. Please <Link to='/'>click here</Link> if you are not
+  automatically redirected.</p>
+
+  const error404 = <p>Sorry, but there's nothing at here at this url. Maybe it moved to a different URL? Please <Link to='/'>click here</Link> to go home, or enter a valid URL.</p> 
+
   return (
     <React.Fragment>
       <h1>Error Code: {errorCode}</h1>
-      <h2>
-        Sorry, there was an error. Please refresh the page if you are not
-        automatically redirected.
-      </h2>
+      <h2>{errorCode === 404 ? error404 : otherError}</h2>
     </React.Fragment>
   );
 };
