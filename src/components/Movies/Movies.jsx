f79d8562b38227a347894b748c0f19f899651d46
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-12 14:10:39 -0700

    Add file for Movies component

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MovieCard from "../MovieCard/MovieCard";
+import Banner from '../Banner/Banner'
+
+const Movies = (props) => {
+  const { movies } = props;
+
+  return (
+    <div>
+      <Banner movies={movies}/>
+      <main className="bg-dark">
+        <div className="card-deck">
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.id}
+              data={movie}
+              getMovieDetails={props.getMovieDetails}
+              {...props}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Movies;
