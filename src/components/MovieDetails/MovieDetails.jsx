fc0e3eae089d254f4da1d20c8f6d548f60ba7a9f
Scott Brabson <scottbrabson@gmail.com>
2020-12-12 18:29:36 -0700

    Add context.color for score, major styling updates

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -31,6 +31,22 @@ const MovieDetails = props => {
     return `https://www.youtube.com/watch?v=${matchedVideo.key}`;
   }
 
+  const getReleaseYear = date => {
+    return release_date.split("-")[0];
+  };
+
+  const getRatingColor = () => {
+    if (rating >= 7) {
+      return { color: "#0ff900" };
+    } else if (rating <= 4.5) {
+      return { color: "#ff5c5c" };
+    } else {
+      return { color: "#f9e600" };
+    }
+  };
+
+  const ratingPercent = ((rating / 10) * 100).toFixed() + "%";
+
   const loading = (
     <div style={{ width: "100vh", height: "100vw", display: "flex" }}>
       <h1 style={{ fontSize: "20em", color: "white" }}>LOADING</h1>
@@ -40,38 +56,48 @@ const MovieDetails = props => {
   onLoad();
 
   return (
-    <section>
+    <React.Fragment>
       {!title ? (
         loading
       ) : (
-        <div>
-          <div className='wrapper'>
+        <div className="page-wrapper">
+          <div className="wrapper">
             <img
               className="img-fluid backdrop kenburns-bottom-left"
               src={backdrop}
               alt=""
             />
           </div>
-          <div className="card-body">
-            <h5 className="card-title text-primary">
-              {title} {((rating / 10) * 100).toFixed() + "%"}
-            </h5>
-            <p className="card-text">
-              <small className="text-white">Release Date {release_date}</small>
-            </p>
-            {tagline && <p>{tagline}</p>}
-            <p>{overview}</p>
-            {genres && genres.map(genre => <p key={genre}>{genre}</p>)}
-            {budget > 0 && <p>Budget: ${budget.toLocaleString()}</p>}
-            {revenue > 0 && <p>Revenue: ${revenue.toLocaleString()}</p>}
-            {runtime > 0 && <p>Runtime: {runtime} minutes</p>}
+          <div className="details-wrapper">
+            <div className="details-text-container">
+              <h1 className="">{title}</h1>
+              <div className="sub-header">
+                <h2 style={getRatingColor()}>{ratingPercent}</h2>
+                <div className="sub-header-release-runtime">
+                  <small>{getReleaseYear(release_date)}</small>
+                  <small>{runtime} min</small>
+                </div>
+              </div>
+
+              {tagline && <p>{tagline}</p>}
+              {overview && <p>{overview}</p>}
+              {genres && genres.map(genre => <p key={genre}>{genre}</p>)}
+              {budget > 0 && <p>Budget: ${budget.toLocaleString()}</p>}
+              {revenue > 0 && <p>Revenue: ${revenue.toLocaleString()}</p>}
+            </div>
             {videos.length && (
-              <ReactPlayer url={getVideo("Trailer")} width="100%" />
+              <div className="video-wrapper">
+                <ReactPlayer
+                  controls={true}
+                  url={getVideo("Trailer")}
+                  wrapper="div"
+                />
+              </div>
             )}
           </div>
         </div>
       )}
-    </section>
+    </React.Fragment>
   );
 };
 
