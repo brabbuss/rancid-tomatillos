dc0d4665ec406e7b0fbe9d12ef424ca23c1286d2
Scott Brabson <scottbrabson@gmail.com>
2020-12-12 19:05:39 -0700

    account for non-youtube videos

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -28,7 +28,7 @@ const MovieDetails = props => {
 
   function getVideo(type) {
     const matchedVideo = videos.find(v => v.type === type);
-    return `https://www.youtube.com/watch?v=${matchedVideo.key}`;
+    return `https://www.${matchedVideo.site.toLowerCase()}.com/watch?v=${matchedVideo.key}`;
   }
 
   const getReleaseYear = date => {
@@ -75,11 +75,11 @@ const MovieDetails = props => {
                 <h2 style={getRatingColor()}>{ratingPercent}</h2>
                 <div className="sub-header-release-runtime">
                   <small>{getReleaseYear(release_date)}</small>
+                  <small style={{fontWeight: 600}}>|</small>
                   <small>{runtime} min</small>
                 </div>
               </div>
-
-              {tagline && <p>{tagline}</p>}
+              <h3>{tagline}</h3>
               {overview && <p>{overview}</p>}
               {genres && genres.map(genre => <p key={genre}>{genre}</p>)}
               {budget > 0 && <p>Budget: ${budget.toLocaleString()}</p>}
