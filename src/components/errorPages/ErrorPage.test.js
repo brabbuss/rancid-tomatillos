005bc71495d9e16919531404ed1252ebb0e9a1b0
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-14 14:40:17 -0700

    Removed unused code

--- src/components/errorPages/ErrorPage.test.js
+++ src/components/errorPages/ErrorPage.test.js
@@ -16,16 +16,6 @@ describe("ErrorPage", () => {
     );
 
     expect(screen.queryByText("Error Code: 400")).toBeInTheDocument();
-    screen.queryByText((content, node) => {
-      const hasText = (node) =>
-        node.textContent ===
-        "Sorry, there was an error. Please click here if you are not automatically redirected.";
-      const nodeHasText = hasText(node);
-      const childrenDontHaveText = Array.from(node.children).every(
-        (child) => !hasText(child)
-      );
-      return nodeHasText && childrenDontHaveText;
-    });
   }),
     it("should have an error message for 404 errors", () => {
       const mockErrorCode = 404;
@@ -37,16 +27,5 @@ describe("ErrorPage", () => {
         </MemoryRouter>
       );
       expect(screen.queryByText("Error Code: 404")).toBeInTheDocument();
-
-      screen.queryByText((content, node) => {
-        const hasText = (node) =>
-          node.textContent ===
-          "Sorry, but there is nothing here at this URL. Maybe it moved to a different URL? Please click here to go home, or enter a valid URL.";
-        const nodeHasText = hasText(node);
-        const childrenDontHaveText = Array.from(node.children).every(
-          (child) => !hasText(child)
-        );
-        return nodeHasText && childrenDontHaveText;
-      });
     });
 });
