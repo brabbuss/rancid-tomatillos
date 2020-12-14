66807ae8d44ce85f6665662a7c700392b6864df5
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-14 06:16:12 -0700

    Add test for tag line

--- src/components/MovieDetails/MovieDetails.test.js
+++ src/components/MovieDetails/MovieDetails.test.js
@@ -52,4 +52,51 @@ describe("MovieDetails", () => {
     expect(screen.getByText("Release Date 2020-09-04")).toBeInTheDocument();
     expect(screen.getByRole("img")).toBeInTheDocument();
   });
+
+  it('should render a tag line if there is one', () => {
+    const movieData =  {
+        id: 718444,
+        title: "Rogue",
+        poster_path: "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+        release_date: "2020-08-20",
+        overview: "Battle-hardened O’Hara leads a lively mercenary team of soldiers on a daring mission: rescue hostages from their captors in remote Africa. But as the mission goes awry and the team is stranded, O’Hara’s squad must face a bloody, brutal encounter with a gang of rebels.",
+        genres: [
+            "Action"
+        ],
+        budget: 0,
+        revenue: 0,
+        runtime: 106,
+        tagline: "When the hunter becomes the prey.",
+        average_rating: 6.428571428571429,
+        selectedMovieVideos: [
+        {
+          id: 243,
+          movie_id: 337401,
+          key: "KK8FHdFluOQ",
+          site: "YouTube",
+          type: "Trailer",
+        },
+        {
+          id: 246,
+          movie_id: 337401,
+          key: "1UXZEGYSwgg",
+          site: "YouTube",
+          type: "Featurette",
+        }]
+
+    }
+      const props = {
+      match: {
+        params: { movie_id: "718444" },
+      }
+    }
+    render(
+      <MovieDetails data={movieData} syncMovieID={jest.fn()} {...props} />
+    )
+    expect(
+      screen.queryByText("When the hunter becomes the prey.")
+    ).toBeInTheDocument();
+  })
+
 });
