63c30029ea62280001331c9aeb297e068c28bc6d
Scott Brabson <scottbrabson@gmail.com>
2020-12-14 20:11:42 -0700

    add fake data to import to searchresults.test.js

--- src/data/fakeMovieData.js
+++ src/data/fakeMovieData.js
@@ -267,6 +267,32 @@ export const fakeMovieData = {
   ]
 };
 
+export const mockSearchResults = [
+  {
+    "id": 694919,
+    "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+    "title": "Money Plane",
+    "average_rating": 6.666666666666667,
+    "release_date": "2020-09-29"
+  },
+  {
+    "id": 337401,
+    "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+    "title": "Mulan",
+    "average_rating": 4.909090909090909,
+    "release_date": "2020-09-04"
+  },{
+    "id": 592350,
+    "poster_path": "https://image.tmdb.org/t/p/original//zGVbrulkupqpbwgiNedkJPyQum4.jpg",
+    "backdrop_path": "https://image.tmdb.org/t/p/original//9guoVF7zayiiUq5ulKQpt375VIy.jpg",
+    "title": "My Hero Academia: Heroes Rising",
+    "average_rating": 5,
+    "release_date": "2019-12-20"
+  }
+]
+
 export const fakeMovieDetails = {
   id: 337401,
   title: "Mulan",
